e7d0e4e439ff8bf9ba1a47f0a6903a8000db22a3
RadicalUser <gurleen.chhatwal@gmail.com>
2025-02-04 17:50:25 +0530

    smallChanges

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-// GSAP se li hai  Animations (AI ki help :)
+// GSAP se li hai  Animations (AI ki help :) for the INRO section
 gsap.from('.hero h1', { opacity: 0, y: -50, duration: 1, delay: 0.5 });
 gsap.from('.hero p', { opacity: 0, y: 50, duration: 1, delay: 1 });
 gsap.from('.hero .btn', { opacity: 0, y: 50, duration: 1, delay: 1.5 });
